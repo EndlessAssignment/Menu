7105ddb767a4e0f817dd5b652fec901bcffa993f
EndlessAssignment <83948372+EndlessAssignment@users.noreply.github.com>
2021-10-27 22:34:29 +0900

    qwer

--- test.py
+++ test.py
@@ -10,7 +10,8 @@ dic = {}
 filtered = []
 
 for i in range(0, len(menu)):
-    if "카페" not in tag[i] and "커피" not in tag[i] and "베이커리" not in tag[i]:
+    if "카페" not in tag[i] and "커피" not in tag[i] and "베이커리" not in tag[i] and "디저트" not in tag[i]\
+            and "브런치" not in tag[i] and "포장마차" not in tag[i]:
         filtered.append(menu[i])
 
 print(random.choice(filtered))
