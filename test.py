ff58c09cae86982e0c5e32f845be68edd2902dc1
EndlessAssignment <83948372+EndlessAssignment@users.noreply.github.com>
2022-01-03 18:04:39 +0900

    코드 간략하게 수정

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ df = pd.read_excel('./menu.xlsx')
 menu = list(df['menu'])
 tag = list(df['tag'])
 rate = list(df['rate'])
+ban = {"카페", "커피", "베이커리", "디저트", "브런치", "포장마차"}
 dic = {}
 filtered = []
 
 for i in range(0, len(menu)):
-    if "카페" not in tag[i] and "커피" not in tag[i] and "베이커리" not in tag[i] and "디저트" not in tag[i]\
-            and "브런치" not in tag[i] and "포장마차" not in tag[i]:
+    tag_list = set(tag[i].split(","))
+    if ban & tag_list == set():
         filtered.append(menu[i])
 
 for i in range(0, 10):
